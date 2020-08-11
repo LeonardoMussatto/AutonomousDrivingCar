f6b128fc07998bf30188f74fba33fc7852834805
Leo <23527613+LeonardoMussatto@users.noreply.github.com>
2020-08-11 17:06:41 +0200

    set the initial state of the Arduino Project - CU-78qv0e[closed]

---
+++ AutonomousDrivingCar/AutonomousDrivingCar/AutonomousDrivingCar.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		AutonomousDrivingCar.ino
+ Created:	8/11/2020 5:02:41 PM
+ Author:	Leo
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
